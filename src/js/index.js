c528277798dc1e72001cbb827336768d9cc04fb6
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-14 16:14:25 -0500

    [Add] the header and the navbar with  mobile first and also the desktop view

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 import '../css/styles.css';
+import '../css/desktop.css';
+
+const menuBtnMobile = document.querySelector('#menu-btn');
+const menuLinks = document.querySelector('.links');
+
+const moreBtn = document.querySelector('.more');
+const moreMiniMenu = document.querySelector('.more .menu');
+
+menuBtnMobile.onclick = () => menuLinks.classList.toggle('active');
+
+
+moreBtn.onclick = () => moreMiniMenu.classList.toggle('active');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 // Rules for Css
 const rulesForCss = {
   test: /\.css$/,
